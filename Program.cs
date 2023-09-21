96539a6cfd9bd4f9d5ac9a862e70b7ce564b72e5
Mikkel Porse <mpr@kfforsikring.dk>
2023-09-21 22:34:48 +0200

    repro

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Security;
+using System.ServiceModel;
+using OpenTelemetry.Instrumentation.Wcf;
+
+namespace OpenTelemetry.Instrumentation.Wcf_bug
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			var binding = new NetTcpBinding(SecurityMode.TransportWithMessageCredential);
+			binding.TransferMode = TransferMode.Buffered;
+			binding.Security.Transport.ProtectionLevel = ProtectionLevel.EncryptAndSign;
+			binding.Security.Message.ClientCredentialType = MessageCredentialType.Windows;
+			
+			EndpointAddress endpoint = new EndpointAddress("net.tcp://anything.will.do:1234");
+			var factory = new ChannelFactory<IDummyService>(binding, endpoint);
+			factory.Endpoint.Behaviors.Add(new TelemetryEndpointBehavior());
+			factory.CreateChannel().Ping();
+		}
+	}
+
+	[ServiceContract]
+	interface IDummyService
+	{
+		[OperationContract]
+		void Ping();
+	}
+}
